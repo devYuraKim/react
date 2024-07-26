87af599a7c115d664e52ff8d7fc6481a0688b4d6
devYuraKim <dev.yurakim@gmail.com>
2024-07-26 23:39:18 +0900

    improve simple useState exercise

--- c01-date-counter/src/App.js
+++ c01-date-counter/src/App.js
@@ -2,27 +2,39 @@ import { useState } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [step, setStep] = useState(1);
 
   const date = new Date("june 21 2027");
   date.setDate(date.getDate() + count);
 
   function handleMinus() {
-    setCount((cur) => cur - 1);
+    setCount((cur) => cur - step);
   }
 
   function handlePlus() {
-    setCount((cur) => cur + 1);
+    setCount((cur) => cur + step);
   }
 
   return (
     <div>
       <div>
-        <button onClick={handleMinus}>-</button>
-        <span>count:{count}</span>
-        <button onClick={handlePlus}>+</button>
+        <div>
+          <button onClick={() => setStep((cur) => cur - 1)}>-</button>
+          <span>step:{step}</span>
+          <button onClick={() => setStep((cur) => cur + 1)}>+</button>
+        </div>
+
+        <div>
+          <button onClick={handleMinus}>-</button>
+          <span>count:{count}</span>
+          <button onClick={handlePlus}>+</button>
+        </div>
 
         <p>
-          {count} days from today is {date.toDateString()}
+          {count === 0 && `today is ${date.toDateString()}`}
+          {count > 0 && `${count} days from today is ${date.toDateString()}`}
+          {count < 0 &&
+            `${count} days ago from today was ${date.toDateString()}`}
         </p>
       </div>
     </div>
