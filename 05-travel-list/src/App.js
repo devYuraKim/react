49b2669173eb0d03446418bfbc52266f9071f78e
devYuraKim <dev.yurakim@gmail.com>
2024-07-29 16:57:28 +0900

    delete with map

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -13,6 +13,10 @@ function App() {
     setItems((items) => [...items, item]);
   }
 
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
   return (
     <div className="app">
       <Logo />
