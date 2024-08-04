160eacb5fbad43b421498e7d6f87e3f8b4ac307d
devYuraKim <dev.yurakim@gmail.com>
2024-08-04 17:05:34 +0900

    useEffect and error management

--- 07-usepopcorn/src/index.js
+++ 07-usepopcorn/src/index.js
@@ -1,29 +1,36 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-// import App from "./App";
-// import "./index.css";
+import App from "./App-v1";
+import "./index.css";
 import StarRating from "./StarRating";
 
-function Test() {
-  const [movieRating, setMovieRating] = useState(0);
+// function Test() {
+//   const [movieRating, setMovieRating] = useState(0);
 
-  return (
-    <div>
-      <StarRating color="teal" maxRating={10} onSetRating={setMovieRating} />
-      <p>This movie was rated {movieRating} stars</p>
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <StarRating color="teal" maxRating={10} onSetRating={setMovieRating} />
+//       <p>This movie was rated {movieRating} stars</p>
+//     </div>
+//   );
+// }
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <React.StrictMode>
+//     {/* <App /> */}
+//     <StarRating
+//       maxRating={5}
+//       messages={["terrible", "bad", "okay", "good", "amazing"]}
+//     />
+//     <StarRating maxRating={5} color="blue" size={24} defaultRating={3} />
+//     <Test />
+//   </React.StrictMode>
+// );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating
-      maxRating={5}
-      messages={["terrible", "bad", "okay", "good", "amazing"]}
-    />
-    <StarRating maxRating={5} color="blue" size={24} defaultRating={3} />
-    <Test />
+    <App />
   </React.StrictMode>
 );
