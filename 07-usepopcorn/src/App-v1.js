d86df60dfb420ba9023660e3919e7aa8e7671a19
devYuraKim <dev.yurakim@gmail.com>
2024-08-06 23:30:47 +0900

    clean up

--- 07-usepopcorn/src/App-v1.js
+++ 07-usepopcorn/src/App-v1.js
@@ -310,8 +310,11 @@ function MovieDetails({ selectedId, setSelectedId, onAddWatched, watched }) {
     function () {
       if (!title) return;
       document.title = `Movie | ${title}`;
+      return function () {
+        document.title = "usePopcorn";
+      };
     },
-    [title, selectedId]
+    [title]
   );
 
   return (
