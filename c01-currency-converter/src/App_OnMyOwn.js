e4e28fc5db5290a23eeba9ed8864ffb026504722
devYuraKim <dev.yurakim@gmail.com>
2024-08-07 20:09:34 +0900

    issues resolved for c01-currency-converter

--- c01-currency-converter/src/App_OnMyOwn.js
+++ c01-currency-converter/src/App_OnMyOwn.js
@@ -3,8 +3,9 @@ import { useState, useEffect } from "react";
 export default function App() {
   const [amount, setAmount] = useState(0);
   const [from, setFrom] = useState("USD");
-  const [to, setTo] = useState("KRW");
+  const [to, setTo] = useState("USD");
   const [result, setResult] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
 
   function handleInput(e) {
     setAmount(e.target.value);
@@ -13,6 +14,7 @@ export default function App() {
   useEffect(
     function () {
       async function fetchData() {
+        setIsLoading(true);
         const host = "api.frankfurter.app";
         const res = await fetch(
           `https://${host}/latest?amount=${amount}&from=${from}&to=${to}`
@@ -20,7 +22,17 @@ export default function App() {
         const data = await res.json();
         const rates = data.rates[to];
         setResult(rates);
-        console.log(result);
+        setIsLoading(false);
+      }
+      if (amount === 0) {
+        setIsLoading(false);
+        setResult(amount);
+        return;
+      }
+      if (to === from) {
+        setIsLoading(false);
+        setResult(amount);
+        return;
       }
       fetchData();
     },
@@ -29,23 +41,35 @@ export default function App() {
 
   return (
     <div className="App">
-      <input type="number" onChange={handleInput} />
-      <select onChange={(e) => setFrom(e.target.value)} value={from}>
+      <input type="number" onChange={handleInput} disabled={isLoading} />
+      <select
+        onChange={(e) => setFrom(e.target.value)}
+        value={from}
+        disabled={isLoading}
+      >
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="KRW">KRW</option>
         <option value="JPY">JPY</option>
       </select>
-      <select onChange={(e) => setTo(e.target.value)} value={to}>
+      <select
+        onChange={(e) => setTo(e.target.value)}
+        value={to}
+        disabled={isLoading}
+      >
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="KRW">KRW</option>
         <option value="JPY">JPY</option>
       </select>
-      <p>OUTPUT</p>
-      <p>
-        {amount} {from} is {result} {to}
-      </p>
+
+      {isLoading ? (
+        <p>⏱️ Please wait. We are fetching the data now...</p>
+      ) : (
+        <p>
+          {from} {amount} is {to} {result}
+        </p>
+      )}
     </div>
   );
 }
