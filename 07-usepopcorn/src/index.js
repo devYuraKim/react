286db11f165afc354bc90a909d0a529b73c4e554
devYuraKim <dev.yurakim@gmail.com>
2024-08-08 22:44:55 +0900

    set up for hooks

--- 07-usepopcorn/src/index.js
+++ 07-usepopcorn/src/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App-v1";
+import App from "./App-v2";
 import "./index.css";
 import StarRating from "./StarRating";
 
