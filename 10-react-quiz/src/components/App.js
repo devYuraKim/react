3881487f9f057f4bcee519bbbd9fd687a2d22b37
devYuraKim <dev.yurakim@gmail.com>
2024-08-12 14:53:56 +0900

    implement state management logic w/ useReducer

--- 10-react-quiz/src/components/App.js
+++ 10-react-quiz/src/components/App.js
@@ -11,6 +11,8 @@ const initialState = {
   //'loading', 'error', 'ready', 'active', 'finished'
   status: "loading",
   index: 0,
+  answer: null,
+  points: 0,
 };
 
 function reducer(state, action) {
@@ -21,13 +23,26 @@ function reducer(state, action) {
       return { ...state, status: "error" };
     case "start":
       return { ...state, status: "active" };
+    case "setIndex":
+      return { ...state, index: action.payload + 1 };
+    case "newAnswer":
+      const question = state.questions.at(state.index);
+
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === question.correctOption
+            ? state.points + question.points
+            : state.points,
+      };
     default:
       throw new Error("Action unknown");
   }
 }
 
 export default function App() {
-  const [{ questions, status, index }, dispatch] = useReducer(
+  const [{ questions, status, index, answer }, dispatch] = useReducer(
     reducer,
     initialState
   );
@@ -54,6 +69,7 @@ export default function App() {
             index={index}
             question={questions[index]}
             dispatch={dispatch}
+            answer={answer}
           />
         )}
       </Main>
--- 10-react-quiz/src/components/Question.js
+++ 10-react-quiz/src/components/Question.js
@@ -1,9 +1,10 @@
 import Options from "./Options";
-function Question({ question }) {
+function Question({ question, dispatch, index, answer }) {
   return (
     <div>
+      <p>{index + 1}</p>
       <h4>{question.question}</h4>
-      <Options question={question} />
+      <Options question={question} dispatch={dispatch} answer={answer} />
     </div>
   );
 }
