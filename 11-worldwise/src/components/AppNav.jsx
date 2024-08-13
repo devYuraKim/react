d1f18e23557bbef0d37548c15cae0ea5c6f3774d
devYuraKim <dev.yurakim@gmail.com>
2024-08-13 19:10:22 +0900

    Nested Route, Index Route, Outlet

--- 11-worldwise/src/components/AppNav.jsx
+++ 11-worldwise/src/components/AppNav.jsx
@@ -1,9 +1,17 @@
+import { NavLink } from "react-router-dom";
 import styles from "./AppNav.module.css";
 
 function AppNav() {
   return (
     <div className={styles.nav}>
-      <h1>AppNavigation</h1>
+      <ul>
+        <li>
+          <NavLink to="cities">도시</NavLink>
+        </li>
+        <li>
+          <NavLink to="countries">국가</NavLink>
+        </li>
+      </ul>
     </div>
   );
 }
