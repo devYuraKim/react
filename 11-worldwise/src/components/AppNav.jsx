380ccd0bc21d572aecfe03b635f33c219f7f4dfc
devYuraKim <dev.yurakim@gmail.com>
2024-08-13 15:15:47 +0900

    introduce css modules

---
+++ 11-worldwise/src/components/AppNav.jsx
@@ -0,0 +1,11 @@
+import styles from "./AppNav.module.css";
+
+function AppNav() {
+  return (
+    <div className={styles.nav}>
+      <h1>AppNavigation</h1>
+    </div>
+  );
+}
+
+export default AppNav;
