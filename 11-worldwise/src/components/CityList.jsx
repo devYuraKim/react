cadf9bb3dbcd2dc9b9c984ef0c9e3b155b430035
devYuraKim <dev.yurakim@gmail.com>
2024-08-28 16:40:16 +0900

    add comment for custom context hook
    actual context consumption

--- 11-worldwise/src/components/CityList.jsx
+++ 11-worldwise/src/components/CityList.jsx
@@ -5,7 +5,9 @@ import Message from "./Message";
 import { useCities } from "../contexts/CitiesContext";
 
 function CityList() {
+  // context consumption
   const { cities, isLoading } = useCities();
+
   if (isLoading) return <Spinner />;
 
   if (cities.length === 0)
