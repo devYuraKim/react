52615e52dea52b17519b984572da611a82120953
devYuraKim <dev.yurakim@gmail.com>
2024-09-02 14:20:30 +0900

    resolve issue #7
    be aware of stale data caused by asynchronous state updates in React, as state changes are not applied immediately

--- Re07-usepopcorn/src/StarRating.js
+++ Re07-usepopcorn/src/StarRating.js
@@ -12,21 +12,19 @@ const starContainerStyle = {
 
 function StarRating({ maxRating = 5 }) {
   const [rating, setRating] = useState(0);
-  const [isFilled, setIsFilled] = useState(false);
 
   function handleRating(i) {
     setRating(i + 1);
-    if (i <= rating) setIsFilled(true);
   }
 
   return (
     <div style={containerStyle}>
       <div style={starContainerStyle}>
         {Array.from({ length: maxRating }, (_, i) => (
-          <Star key={i} onClick={() => handleRating(i)} isFilled={isFilled} />
+          <Star key={i} onClick={() => handleRating(i)} isFilled={rating > i} />
         ))}
       </div>
-      <p>{rating}</p>
+      <p>{rating || ""}</p>
     </div>
   );
 }
@@ -61,7 +59,6 @@ function Star({ onClick, isFilled }) {
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 20 20"
-          fill="white"
           stroke="black"
         >
           <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
