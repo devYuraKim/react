61c882c45336048cba9d47e2954781e2406a7ae7
devYuraKim <dev.yurakim@gmail.com>
2024-09-04 13:55:24 +0900

    issue #10 setting up custom hook

--- Re07-usepopcorn/src/App.js
+++ Re07-usepopcorn/src/App.js
@@ -1,4 +1,5 @@
 import { useState, useEffect, useRef } from "react";
+import useMovies from "./hooks/useMovies";
 import StarRating from "./StarRating.js";
 
 const average = (arr) =>
@@ -8,14 +9,13 @@ const KEY = "d9c29e1c";
 
 export default function App() {
   const [query, setQuery] = useState("");
-  const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState(function () {
     const storedValue = localStorage.getItem("watched");
     return storedValue ? JSON.parse(storedValue) : [];
   });
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState("");
   const [selectedId, setSelectedId] = useState(null);
+
+  const { movies, isLoading, error } = useMovies(query, setSelectedId);
 
   function handleSelectMovie(id) {
     setSelectedId((curId) => (id === curId ? null : id));
@@ -34,49 +34,6 @@ export default function App() {
       localStorage.setItem("watched", JSON.stringify(watched));
     },
     [watched]
-  );
-
-  useEffect(
-    function () {
-      const controller = new AbortController();
-      async function fetchMovies() {
-        try {
-          setError(null);
-          setIsLoading(true);
-          const res = await fetch(
-            `https://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
-            { signal: controller.signal }
-          );
-          if (!res.ok) throw new Error("Lost Connection");
-
-          const data = await res.json();
-          if (data.Response === "False") throw new Error("No Result");
-
-          setMovies(data.Search);
-          setIsLoading(false);
-          setError(null);
-        } catch (err) {
-          if (err.name !== "AbortError") {
-            console.log(err.message);
-            setError(err.message);
-          }
-        } finally {
-          setIsLoading(false);
-        }
-      }
-      if (!query.length) {
-        setMovies([]);
-        setError(null);
-        return;
-      }
-      setSelectedId(null);
-      fetchMovies();
-
-      return function () {
-        controller.abort();
-      };
-    },
-    [query]
   );
 
   return (
