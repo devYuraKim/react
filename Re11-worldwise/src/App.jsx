4ad7f48b2947984213cf81d5a3409f17d2993a91
devYuraKim <dev.yurakim@gmail.com>
2024-09-07 21:50:10 +0900

    base code for worldwise review

---
+++ Re11-worldwise/src/App.jsx
@@ -0,0 +1,33 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import Homepage from "./pages/Homepage";
+import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
+import Login from "./pages/Login";
+import City from "./components/City";
+import Form from "./components/Form";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Homepage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="app" element={<AppLayout />}>
+          <Route index element={<Navigate replace to="cities" />} />
+          <Route path="cities" element={<p>City List</p>} />
+          <Route path="cities/:id" element={<City />} />
+          <Route path="countries" element={<p>Country List</p>} />
+          <Route path="form" element={<Form />} />
+        </Route>
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
