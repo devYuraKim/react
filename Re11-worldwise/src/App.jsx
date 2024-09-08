bfdae35b8b3fa57ab4136812c970a6d98b7a3f6b
devYuraKim <dev.yurakim@gmail.com>
2024-09-08 17:07:19 +0900

    issue #16 unable to render PageNav within App

--- Re11-worldwise/src/App.jsx
+++ Re11-worldwise/src/App.jsx
@@ -1,32 +1,26 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import Homepage from "./pages/Homepage";
 import PageNotFound from "./pages/PageNotFound";
-import AppLayout from "./pages/AppLayout";
-import Login from "./pages/Login";
-import City from "./components/City";
-import Form from "./components/Form";
+import PageNav from "./components/PageNav";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<Homepage />} />
-        <Route path="product" element={<Product />} />
-        <Route path="pricing" element={<Pricing />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="app" element={<AppLayout />}>
-          <Route index element={<Navigate replace to="cities" />} />
-          <Route path="cities" element={<p>City List</p>} />
-          <Route path="cities/:id" element={<City />} />
-          <Route path="countries" element={<p>Country List</p>} />
-          <Route path="form" element={<Form />} />
-        </Route>
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <PageNav />
+      <p>constant element</p>
+
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Homepage />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/pricing" element={<Pricing />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
