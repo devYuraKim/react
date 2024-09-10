ea1f9a9d38d67b29950998043e989833b0de5b29
devYuraKim <dev.yurakim@gmail.com>
2024-09-10 13:32:14 +0900

    alternative: use Link to navigate

--- Re11-worldwise/src/components/CityItem.jsx
+++ Re11-worldwise/src/components/CityItem.jsx
@@ -1,5 +1,5 @@
 import styles from "./CityItem.module.css";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import City from "./City";
 
@@ -11,14 +11,14 @@ const formatDate = (date) =>
   }).format(new Date(date));
 
 function CityItem({ city }) {
-  const navigate = useNavigate();
-
   return (
-    <li className={styles.cityItem} onClick={() => navigate(`${city.id}`)}>
-      <span className={styles.emoji}>{city.emoji}</span>
-      <h3 className={styles.name}>{city.cityName}</h3>
-      <time className={styles.date}> ({formatDate(city.date)}) </time>
-      <button className={styles.deleteBtn}>&times;</button>
+    <li>
+      <Link className={styles.cityItem} to={`${city.id}`}>
+        <span className={styles.emoji}>{city.emoji}</span>
+        <h3 className={styles.name}>{city.cityName}</h3>
+        <time className={styles.date}> ({formatDate(city.date)}) </time>
+        <button className={styles.deleteBtn}>&times;</button>
+      </Link>
     </li>
   );
 }
