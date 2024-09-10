7d419bb1ae3cb07cf3287655e257da3b31c0449b
devYuraKim <dev.yurakim@gmail.com>
2024-09-10 17:01:58 +0900

    implement keydown event
    - point1: useRef or document.addEventListener
    - point2: clean up function removeEventListener()
    
    I initially thought of using useRef (adding ref prop to <div>) because it’s often used for directly interacting with DOM elements.
    >> If I were adding an event listener specifically to a particular <div> and not to the entire document, using useRef would be appropriate.

--- Re11-worldwise/src/App.jsx
+++ Re11-worldwise/src/App.jsx
@@ -45,7 +45,7 @@ function App() {
           <Route path="/product" element={<Product />} />
           <Route path="/pricing" element={<Pricing />} />
           <Route path="/app" element={<AppLayout />}>
-            <Route index element={<Navigate to="cities" />} />
+            <Route index element={<Navigate to="cities" replace />} />
             <Route
               path="cities"
               element={<CityList cities={cities} isLoading={isLoading} />}
--- Re11-worldwise/src/pages/AppLayout.jsx
+++ Re11-worldwise/src/pages/AppLayout.jsx
@@ -2,8 +2,25 @@ import styles from "./AppLayout.module.css";
 import Sidebar from "../components/Sidebar";
 import User from "../components/User";
 import Map from "../components/Map";
+import { useEffect, useRef } from "react";
+import { useNavigate } from "react-router-dom";
 
 function AppLayout() {
+  const navigate = useNavigate();
+
+  useEffect(
+    function () {
+      function useBackspace(e) {
+        if (e.code === "Backspace") navigate(-1);
+      }
+
+      document.addEventListener("keydown", useBackspace);
+
+      return () => document.removeEventListener("keydown", useBackspace);
+    },
+    [navigate]
+  );
+
   return (
     <div className={styles.app}>
       <Sidebar />
