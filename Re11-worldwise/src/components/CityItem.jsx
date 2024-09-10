08949566655ba3a9c922d418fa1206a04f1f2e0d
devYuraKim <dev.yurakim@gmail.com>
2024-09-10 12:31:35 +0900

    issue #17 incorrect use of Navigate component

--- Re11-worldwise/src/components/CityItem.jsx
+++ Re11-worldwise/src/components/CityItem.jsx
@@ -1,4 +1,7 @@
 import styles from "./CityItem.module.css";
+import { Navigate } from "react-router-dom";
+
+import City from "./City";
 
 const formatDate = (date) =>
   new Intl.DateTimeFormat("en", {
@@ -8,8 +11,11 @@ const formatDate = (date) =>
   }).format(new Date(date));
 
 function CityItem({ city }) {
+  function handleClick() {
+    return <Navigate to={city.id} element={<City />} />;
+  }
   return (
-    <li className={styles.cityItem}>
+    <li className={styles.cityItem} onClick={handleClick}>
       <span className={styles.emoji}>{city.emoji}</span>
       <h3 className={styles.name}>{city.cityName}</h3>
       <time className={styles.date}> ({formatDate(city.date)}) </time>
