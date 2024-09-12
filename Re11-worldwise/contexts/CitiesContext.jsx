919c6dd220597ee634f4c9b0fca1e9b8e5262f8f
devYuraKim <dev.yurakim@gmail.com>
2024-09-12 13:38:15 +0900

    issue #20 unable to get id

--- Re11-worldwise/contexts/CitiesContext.jsx
+++ Re11-worldwise/contexts/CitiesContext.jsx
@@ -8,6 +8,7 @@ const CitiesContext = createContext();
 function CitiesProvider({ children }) {
   const [cities, setCities] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [currentCity, setCurrentCity] = useState({});
 
   useEffect(function () {
     async function fetchCities() {
@@ -25,8 +26,24 @@ function CitiesProvider({ children }) {
     fetchCities();
   }, []);
 
+  async function getCurrentCity(id) {
+    setIsLoading(true);
+    try {
+      //이 URL이면 특정 city 객체만 반환됨
+      const res = await fetch(`${SERVER_URL}/${id}`);
+      const data = await res.json();
+      setCurrentCity(data);
+    } catch (err) {
+      console.log(err.message);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
   return (
-    <CitiesContext.Provider value={{ cities, isLoading }}>
+    <CitiesContext.Provider
+      value={{ cities, isLoading, currentCity, getCurrentCity }}
+    >
       {children}
     </CitiesContext.Provider>
   );
--- Re11-worldwise/src/components/City.jsx
+++ Re11-worldwise/src/components/City.jsx
@@ -1,75 +1,63 @@
-import { useParams, useSearchParams } from "react-router-dom";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useCities } from "../../contexts/CitiesContext";
 import styles from "./City.module.css";
 
-function City({ city }) {
-  // TEMP DATA
-  const currentCity = {
-    cityName: "Lisbon",
-    emoji: "🇵🇹",
-    date: "2027-10-31T15:59:59.138Z",
-    notes: "My favorite city so far!",
-  };
+const formatDate = (date) =>
+  new Intl.DateTimeFormat("en", {
+    day: "numeric",
+    month: "short",
+    year: "numeric",
+  }).format(new Date(date));
 
-  const { cityName, emoji, date, notes } = currentCity;
+function City() {
+  const { id } = useParams();
+  const { currentCity, getCurrentCity } = useCities();
 
-  const { cityId } = useParams();
+  useEffect(
+    function () {
+      getCurrentCity(id);
+      console.log(currentCity);
+    },
+    [id]
+  );
 
-  const [searchParams, setSearchParams] = useSearchParams();
-  const lat = searchParams.get("lat");
-  const lng = searchParams.get("lng");
+  const { emoji, cityName, date, notes } = currentCity;
 
   return (
     <div className={styles.city}>
-      <h1>{cityId}</h1>
-      <h1>
-        {lat}, {lng}
-      </h1>
+      <div className={styles.row}>
+        <h6>City name</h6>
+        <h3>
+          <span>{emoji}</span> {cityName}
+        </h3>
+      </div>
 
-      <button
-        onClick={() =>
-          setSearchParams({
-            lat: 0,
-            lng: lng,
-          })
-        }
-      >
-        Change Latitude
-      </button>
+      <div className={styles.row}>
+        <h6>You went to {cityName} on</h6>
+        <p>{formatDate(date || null)}</p>
+      </div>
+
+      {notes && (
+        <div className={styles.row}>
+          <h6>Your notes</h6>
+          <p>{notes}</p>
+        </div>
+      )}
+
+      <div className={styles.row}>
+        <h6>Learn more</h6>
+        <a
+          href={`https://en.wikipedia.org/wiki/${cityName}`}
+          target="_blank"
+          rel="noreferrer"
+        >
+          Check out {cityName} on Wikipedia &rarr;
+        </a>
+      </div>
+
+      <div>{/* <ButtonBack /> */}</div>
     </div>
-
-    //   <div className={styles.city}>
-    //     <div className={styles.row}>
-    //       <h6>City name</h6>
-    //       <h3>
-    //         <span>{emoji}</span> {cityName}
-    //       </h3>
-    //     </div>
-
-    //     <div className={styles.row}>
-    //       <h6>You went to {cityName} on</h6>
-    //       <p>{formatDate(date || null)}</p>
-    //     </div>
-
-    //     {notes && (
-    //       <div className={styles.row}>
-    //         <h6>Your notes</h6>
-    //         <p>{notes}</p>
-    //       </div>
-    //     )}
-
-    //     <div className={styles.row}>
-    //       <h6>Learn more</h6>
-    //       <a
-    //         href={`https://en.wikipedia.org/wiki/${cityName}`}
-    //         target="_blank"
-    //         rel="noreferrer"
-    //       >
-    //         Check out {cityName} on Wikipedia &rarr;
-    //       </a>
-    //     </div>
-
-    //     <div>{/* <ButtonBack /> */}</div>
-    //   </div>
   );
 }
 
