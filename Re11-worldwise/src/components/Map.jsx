956e1cf16bc56bd31b31c0c9f671c8dd269792c7
devYuraKim <dev.yurakim@gmail.com>
2024-09-12 16:44:46 +0900

    issue #21 unable to set default lat, lng

--- Re11-worldwise/src/components/Map.jsx
+++ Re11-worldwise/src/components/Map.jsx
@@ -1,6 +1,7 @@
+import { useEffect } from "react";
 import { useSearchParams, useNavigate } from "react-router-dom";
 import styles from "./Map.module.css";
-import { MapContainer, TileLayer, useMap, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
 function Map() {
   const navigate = useNavigate();
@@ -9,24 +10,36 @@ function Map() {
   const lat = searchParams.get("lat");
   const lng = searchParams.get("lng");
 
+  useEffect(
+    function () {
+      const defaultLat = lat || "40.7128";
+      const defaultLng = lng || "-74.0060";
+
+      if (!lat || !lng) {
+        setSearchParams({ lat: defaultLat, lng: defaultLng });
+      }
+    }[(lat, lng, setSearchParams)]
+  );
+
   return (
-    <MapContainer
-      className={styles.mapContainer}
-      onClick={() => navigate("form")}
-      center={[51.505, -0.09]}
-      zoom={13}
-      scrollWheelZoom={false}
-    >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
-    </MapContainer>
+    <div className={styles.mapContainer} onClick={() => navigate("form")}>
+      <MapContainer
+        className={styles.map}
+        center={[lat || 40.7128, lng || -74.006]}
+        zoom={5}
+        scrollWheelZoom={false}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[lat || 40.7128, lng || -74.006]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
   );
 }
 
