e7f18ce38b78e3381a31f7698e19dffb0fbe54c5
devYuraKim <dev.yurakim@gmail.com>
2024-09-15 12:42:13 +0900

    feedback - useReducer syntax
    *reducer(action, state)
    >>reducer(state, action)

--- Re11-worldwise/contexts/CitiesContext.jsx
+++ Re11-worldwise/contexts/CitiesContext.jsx
@@ -9,60 +9,85 @@ const initialState = {
   cities: [],
   isLoading: false,
   currentCity: {},
+  error: null,
 };
 
-function reducer(action, state) {
+function reducer(state, action) {
   switch (action.type) {
+    case "loading":
+      return { ...state, isLoading: true, error: null };
     case "cities/loaded":
+      return {
+        ...state,
+        isLoading: false,
+        cities: action.payload,
+      };
+    case "city/loaded":
+      return {
+        ...state,
+        isLoading: false,
+        currentCity: action.payload,
+      };
+    case "city/created":
       return {
         ...state,
         isLoading: false,
         cities: [...state.cities, action.payload],
       };
+    case "city/deleted":
+      return {
+        ...state,
+        isLoading: false,
+        cities: [...state.cities.filter((city) => action.payload !== city.id)],
+      };
+    case "rejected":
+      return { ...state, isLoading: false, error: action.payload };
+
     default:
-      return null;
+      throw new Error("unknown action type");
   }
 }
 
 function CitiesProvider({ children }) {
-  const [state, dispatch] = useReducer(reducer, initialState);
-
-  const [cities, setCities] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [currentCity, setCurrentCity] = useState({});
+  const [{ cities, isLoading, currentCity }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
 
   useEffect(function () {
     async function fetchCities() {
-      setIsLoading(true);
+      dispatch({ type: "loading" });
       try {
         const res = await fetch(SERVER_URL);
         const data = await res.json();
-        setCities(data);
-      } catch (err) {
-        console.log(err.message);
-      } finally {
-        setIsLoading(false);
+        dispatch({ type: "cities/loaded", payload: data });
+      } catch {
+        dispatch({
+          type: "rejected",
+          payload: "There was an error loading the cities",
+        });
       }
     }
     fetchCities();
   }, []);
 
   async function getCurrentCity(id) {
-    setIsLoading(true);
+    dispatch({ type: "loading" });
     try {
       //이 URL이면 특정 city 객체만 반환됨
       const res = await fetch(`${SERVER_URL}/${id}`);
       const data = await res.json();
-      setCurrentCity(data);
-    } catch (err) {
-      console.log(err.message);
-    } finally {
-      setIsLoading(false);
+      dispatch({ type: "city/loaded", payload: data });
+    } catch {
+      dispatch({
+        type: "rejected",
+        payload: "There was an error loading the city",
+      });
     }
   }
 
   async function createCity(newCity) {
-    setIsLoading(true);
+    dispatch({ type: "loading" });
     try {
       const res = await fetch(`${SERVER_URL}`, {
         method: "post",
@@ -70,25 +95,27 @@ function CitiesProvider({ children }) {
         headers: { "Content-Type": "application/json" },
       });
       const data = await res.json();
-      setCities((cities) => [...cities, data]);
-    } catch (err) {
-      console.log(err.message);
-    } finally {
-      setIsLoading(false);
+      dispatch({ type: "city/created", payload: data });
+    } catch {
+      dispatch({
+        type: "rejected",
+        payload: "There was an error creating the city",
+      });
     }
   }
 
   async function deleteCity(id) {
-    setIsLoading(true);
+    dispatch({ type: "loading" });
     try {
       await fetch(`${SERVER_URL}`, {
         method: "delete",
       });
+      dispatch({ type: "city/deleted", payload: id });
     } catch (err) {
-      console.log(err.message);
-    } finally {
-      setIsLoading(false);
-      setCities((cities) => cities.filter((city) => id !== city.id));
+      dispatch({
+        type: "rejected",
+        payload: "There was an error deleting the city",
+      });
     }
   }
 
