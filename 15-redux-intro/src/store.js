c72a452314dcaa7ead0f730d75b8b86c620c98f4
devYuraKim <dev.yurakim@gmail.com>
2024-09-23 17:39:28 +0900

    redux basics: createStore, action creators

---
+++ 15-redux-intro/src/store.js
@@ -0,0 +1,70 @@
+//WARNING: use createStore for learning purpose only
+import { createStore } from "redux";
+
+const initialState = {
+  balance: 0,
+  loan: 0,
+  loanPurpose: "",
+};
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case "account/deposit":
+      return { ...state, balance: state.balance + action.payload };
+    case "account/withdraw":
+      return { ...state, balance: state.balance - action.payload };
+    case "account/requestLoan":
+      if (state.loan === 0)
+        return {
+          ...state,
+          loan: action.payload.amount,
+          balance: state.balance + action.payload.amount,
+          loanPurpose: action.payload.purpose,
+        };
+      return state;
+    case "account/payLoan":
+      if (state.loan === 0) return state;
+      return {
+        ...state,
+        loan: 0,
+        loanPurpose: "",
+        balance: state.balance - state.loan,
+      };
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer);
+
+// store.dispatch({ type: "account/deposit", payload: 500 });
+// store.dispatch({ type: "account/withdraw", payload: 200 });
+// store.dispatch({
+//   type: "account/requestLoan",
+//   payload: { amount: 5000, purpose: "purchase a vehicle" },
+// });
+// store.dispatch({ type: "account/payLoan" });
+// console.log(store.getState());
+
+function deposit(amount) {
+  return { type: "account/deposit", payload: amount };
+}
+function withdraw(amount) {
+  return { type: "account/withdraw", payload: amount };
+}
+function requestLoan(amount, purpose) {
+  return {
+    type: "account/requestLoan",
+    payload: { amount: amount, purpose: purpose },
+  };
+}
+function payLoan() {
+  return {
+    type: "account/payLoan",
+  };
+}
+
+store.dispatch(deposit(500));
+store.dispatch(withdraw(200));
+store.dispatch(requestLoan(5000, "purchase a vehicle"));
+store.dispatch(payLoan());
