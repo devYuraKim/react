826bb3ffada5c247e29e2374e4bff1931b7848b7
devYuraKim <dev.yurakim@gmail.com>
2024-09-23 18:41:13 +0900

    export store and import it to index.js

--- 15-redux-intro/src/store.js
+++ 15-redux-intro/src/store.js
@@ -1,15 +1,7 @@
 //WARNING: use createStore for learning purpose only
 import { combineReducers, createStore } from "redux";
-import accountReducer, {
-  deposit,
-  withdraw,
-  requestLoan,
-  payLoan,
-} from "./features/accounts/accountSlice";
-import customerReducer, {
-  createCustomer,
-  updateName,
-} from "./features/customers/customerSlice";
+import accountReducer from "./features/accounts/accountSlice";
+import customerReducer from "./features/customers/customerSlice";
 
 const rootReducer = combineReducers({
   account: accountReducer,
@@ -17,12 +9,4 @@ const rootReducer = combineReducers({
 });
 const store = createStore(rootReducer);
 
-store.dispatch(deposit(500));
-store.dispatch(withdraw(200));
-store.dispatch(requestLoan(5000, "purchase a vehicle"));
-store.dispatch(payLoan());
-
-store.dispatch(createCustomer("John Doe", "RDSSN1"));
-store.dispatch(updateName("John Smith"));
-
-console.log(store.getState());
+export default store;
