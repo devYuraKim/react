1575dd2bcfae8cb0b81a634c445219659924c52a
devYuraKim <dev.yurakim@gmail.com>
2024-09-24 21:03:33 +0900

    implement createSlice from redux toolkit

--- 15-redux-intro/src/features/accounts/accountSlice.js
+++ 15-redux-intro/src/features/accounts/accountSlice.js
@@ -18,18 +18,27 @@ const accountSlice = createSlice({
     withdraw(state, action) {
       state.balance -= action.payload;
     },
-    requestLoan(state, action) {
-      if (state.loan > 0) return;
-      state.loan = action.payload.amount;
-      state.loanPurpose = action.payload.purpose;
-      state.balance = state.balance + action.payload.amount;
+    requestLoan: {
+      //set action creator to receive more than 1 argument
+      prepare(amount, purpose) {
+        return {
+          payload: { amount, purpose },
+        };
+      },
+      reducer(state, action) {
+        if (state.loan > 0) return;
+        state.loan = action.payload.amount;
+        state.loanPurpose = action.payload.purpose;
+        state.balance = state.balance + action.payload.amount;
+      },
     },
     payLoan(state, action) {
-      state.loan = 0;
       state.loanPurpose = "";
       state.balance -= state.loan;
+      state.loan = 0;
     },
   },
 });
 
-console.log(accountSlice);
+export default accountSlice.reducer;
+export const { deposit, withdraw, requestLoan, payLoan } = accountSlice.actions;
