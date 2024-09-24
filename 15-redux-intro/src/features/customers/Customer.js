941590d6f28f1f6396332089086e2ec44b5e9d5d
devYuraKim <dev.yurakim@gmail.com>
2024-09-24 10:16:20 +0900

    incorporate redux and refactor customer feature

--- 15-redux-intro/src/features/customers/Customer.js
+++ 15-redux-intro/src/features/customers/Customer.js
@@ -1,5 +1,11 @@
+import { useSelector } from "react-redux";
+
 function Customer() {
-  return <h2>👋 Welcome, %NAME%</h2>;
+  const { fullName, nationalId, createdAt } = useSelector(
+    (store) => store.customer
+  );
+  // if (!fullName) return <h2>You have not yet created account with us</h2>;
+  return <h2>👋 Welcome, {fullName}</h2>;
 }
 
 export default Customer;
