c54cb54c926d55ff54e0ddcd0074efc7ce3ef85e
devYuraKim <dev.yurakim@gmail.com>
2024-09-24 19:04:50 +0900

    resolve partially issue #25

--- 15-redux-intro/src/store.js
+++ 15-redux-intro/src/store.js
@@ -1,15 +1,24 @@
-//replace {createStore} with {configureStore}
-import { configureStore } from "@reduxjs/toolkit";
-import { composeWithDevTools } from "remote-redux-devtools";
-
 import accountReducer from "./features/accounts/accountSlice";
 import customerReducer from "./features/customers/customerSlice";
+import devToolsEnhancer from "remote-redux-devtools";
+import { configureStore } from "@reduxjs/toolkit";
 
 const store = configureStore({
   reducer: {
     account: accountReducer,
     customer: customerReducer,
   },
+  devTools: false,
+  //with extra enhancers setting, Redux Devtools works on Safari but not on Chrome
+  enhancers: (getDefaultEnhancers) =>
+    getDefaultEnhancers().concat(
+      devToolsEnhancer({
+        realtime: true,
+        name: "redux intro",
+        hostname: "localhost",
+        port: 8000,
+      })
+    ),
 });
 
 export default store;
