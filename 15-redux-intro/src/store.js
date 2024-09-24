ac8d09ccfcc6fb9ad80d43d75f55ddc9383ba2f4
devYuraKim <dev.yurakim@gmail.com>
2024-09-24 15:21:40 +0900

    incorporate Redux Devtools

--- 15-redux-intro/src/store.js
+++ 15-redux-intro/src/store.js
@@ -3,11 +3,23 @@ import { applyMiddleware, combineReducers, createStore } from "redux";
 import { thunk } from "redux-thunk";
 import accountReducer from "./features/accounts/accountSlice";
 import customerReducer from "./features/customers/customerSlice";
+import { composeWithDevTools } from "remote-redux-devtools";
 
 const rootReducer = combineReducers({
   account: accountReducer,
   customer: customerReducer,
 });
-const store = createStore(rootReducer, applyMiddleware(thunk));
+
+const composeEnhancers = composeWithDevTools({
+  realtime: true,
+  name: "redux intro",
+  hostname: "localhost",
+  port: 8000,
+});
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
 
 export default store;
