bbdd0a20a13ac13758ea2a39ff08835a42ba4cb4
devYuraKim <dev.yurakim@gmail.com>
2024-09-29 23:51:09 +0900

    implement useLoaderData

--- 16-fast-react-pizza/src/features/menu/Menu.jsx
+++ 16-fast-react-pizza/src/features/menu/Menu.jsx
@@ -1,5 +1,32 @@
+import { useLoaderData } from "react-router-dom";
+import { getMenu } from "../../services/apiRestaurant";
+
 function Menu() {
-  return <h1>Menu</h1>;
+  const menu = useLoaderData();
+  return (
+    <>
+      <h1>Menu</h1>
+      {menu.map((item) => (
+        <div key={item.id}>
+          <h3>{item.name}</h3>
+          <img src={item.imageUrl} />
+          <p>${item.unitPrice}</p>
+          <p>
+            {item.ingredients.map((ingredient, i) =>
+              ingredients.length - 1 < i ? `${ingredient}, ` : { ingredient }
+            )}
+          </p>
+          {item.soldOut && <p>SOLD OUT</p>}
+        </div>
+      ))}
+    </>
+  );
+}
+
+//fetch the data and then return it
+export async function loader() {
+  const menu = await getMenu();
+  return menu;
 }
 
 export default Menu;
